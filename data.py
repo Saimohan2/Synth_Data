e73a6c925e065782912003cd087e8ac6afdd5d34
Saimohan2 <mohanadabala64@gmail.com>
2026-04-22 15:12:53 -0400

    Numpy feature (#3)
    * reduced parquet size
    
    * Built a sample but decent incremental medallion architecture pipeline
    
    * generated more orders for incremental test
    
    * generated new file
    
    * incr read
    
    * test

--- data.py
+++ data.py
@@ -3,13 +3,15 @@ import pandas as pd
 from faker import Faker
 
 n = 1500000
-num_customers = 10000
+num_customers = 20000
 num_products = 500
 payment_modes = ["Card", " Card ", "caSh", "netbankIng", "Cod", "COD", "UPI"]
 fake = Faker()
 
-order_ids = np.arange(1, n+1)
-customer_ids = np.random.randint(1, num_customers+1, size = n)
+#test
+
+order_ids = np.arange(2250000, 3750000)
+customer_ids = np.random.randint(10001, num_customers+1, size = n)
 product_ids = np.random.randint(1, num_products+1, size = n)
 amounts = np.random.normal(loc = 2000, scale = 500, size = n)
 
@@ -18,8 +20,8 @@ payment_mode = np.random.choice(
     size = n,
     p = [0.2, 0.05, 0.05, 0.3, 0.1, 0.1, 0.2])
 
-start_date = pd.Timestamp("2023-01-01")
-end_date = pd.Timestamp("2025-01-01")
+start_date = pd.Timestamp("2024-01-01")
+end_date = pd.Timestamp("2026-12-31")
 
 days_range = (end_date - start_date).days
 random_days = np.random.randint(0, days_range, size = n)
@@ -37,4 +39,4 @@ orders_df = pd.DataFrame({
 
 print(len(orders_df))
 
-orders_df.to_parquet("orders_04-21.parquet", engine = "pyarrow", index = False)
+orders_df.to_parquet("orders_04-22_2.parquet", engine = "pyarrow", index = False)
