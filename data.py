e31a5f17332cbbf63f80521497f7d89c7b609c91
Saimohan2 <mohanadabala64@gmail.com>
2026-04-22 17:58:23 +0000

    generated more orders for incremental test

--- data.py
+++ data.py
@@ -8,7 +8,7 @@ num_products = 500
 payment_modes = ["Card", " Card ", "caSh", "netbankIng", "Cod", "COD", "UPI"]
 fake = Faker()
 
-order_ids = np.arange(1, n+1)
+order_ids = np.arange(n//2, 2250000)
 customer_ids = np.random.randint(1, num_customers+1, size = n)
 product_ids = np.random.randint(1, num_products+1, size = n)
 amounts = np.random.normal(loc = 2000, scale = 500, size = n)
@@ -18,8 +18,8 @@ payment_mode = np.random.choice(
     size = n,
     p = [0.2, 0.05, 0.05, 0.3, 0.1, 0.1, 0.2])
 
-start_date = pd.Timestamp("2023-01-01")
-end_date = pd.Timestamp("2025-01-01")
+start_date = pd.Timestamp("2024-01-01")
+end_date = pd.Timestamp("2026-01-01")
 
 days_range = (end_date - start_date).days
 random_days = np.random.randint(0, days_range, size = n)
@@ -37,4 +37,4 @@ orders_df = pd.DataFrame({
 
 print(len(orders_df))
 
-orders_df.to_parquet("orders_04-21.parquet", engine = "pyarrow", index = False)
+orders_df.to_parquet("orders_04-22.parquet", engine = "pyarrow", index = False)
